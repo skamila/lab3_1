86790b889c6a05bb1645b68b3dc6b5fb22ea6d4f
struszczak.kamila <209472@edu.p.lodz.pl>
2019-04-08 22:46:11 +0200

    isolation dodanie testu amountOfCallMethodCalculateTax()

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -5,6 +5,7 @@ import org.hamcrest.core.Is;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 import java.math.BigDecimal;
@@ -27,5 +28,25 @@ public class BookKeeperTest {
         assertThat(result.getItems().size(), Is.is(1));
 
     }
+
+
+    @Test
+    public void amountOfCallMethodCalculateTax() {
+
+        BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
+        ClientData clientData = new ClientData(new Id("1"), "Kowalski");
+        InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+        invoiceRequest.add(new RequestItem((mock(ProductData.class)), 1, new Money(new BigDecimal(10.5))));
+        invoiceRequest.add(new RequestItem((mock(ProductData.class)), 2, new Money(new BigDecimal(15.5))));
+        TaxPolicy taxPolicyMock = mock(TaxPolicy.class);
+
+        when(taxPolicyMock.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(new Tax(new Money(new BigDecimal(0.5)), "desctiption"));
+
+        bookKeeper.issuance(invoiceRequest, taxPolicyMock);
+
+        verify(taxPolicyMock, times(2)).calculateTax(any(ProductType.class), any(Money.class));
+
+    }
+
 
 }
